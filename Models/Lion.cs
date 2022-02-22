ef58375e3a3593dd6e06cb71deb09b7d55bbc105
sswanson12 <sswanson12@my.wctc.edu>
2022-02-21 20:48:41 -0600

    Updated with my own Lion Class, and an added switch to give the user the choice of any animal.

---
+++ Models/Lion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractExample.Models
+{
+    public class Lion : Animal
+    {
+        public override void MakeNoise()
+        {
+            Console.WriteLine("RAWWRRRRRR");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine("prrrr.... prrrrr...");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,19 +7,21 @@ namespace AbstractExample
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Which animal do you want to hear?");
-            var choice = Console.ReadLine();
+            Console.WriteLine("Which animal do you want to hear?\n" +
+                              "1. Bird\n2. Cat\n3. Dog\n4. Lion (My own Animal class\n5. Pig");
+            var choice = Convert.ToInt32(Console.ReadLine());
 
-            Animal animal = null;
-
-            if (choice == "1")
+            //The editor simply suggested this as an action when I wrote out a whole switch statement. BUt it's pretty
+            //cool and I'll remember its a thing because that can be handy sometimes.
+            Animal animal = choice switch
             {
-                animal = new Pig();
-            }
-            else if (choice == "2")
-            {
-                animal = new Dog();
-            }
+                1 => new Bird(),
+                2 => new Cat(),
+                3 => new Dog(),
+                4 => new Lion(),//My Class
+                5 => new Pig(),
+                _ => null
+            };
 
             animal?.MakeNoise();
             animal?.Sleep();
